93fc389d30d6d5c01edff465f98868627ad5eec0
Paulo Alexandre <paulo.onf@gmail.com>
2026-09-11 07:25:34 -0300

    add colisao com bordas da tela

--- main.py
+++ main.py
@@ -20,18 +20,25 @@ while running:
     dt = clock.tick(60) / 1000.0  # Delta time in seconds
 
     if lastKeyPressed == pg.K_w:
-        player_pos.y -= 300 * dt
+        player_pos.y -= 500 * dt
     if lastKeyPressed == pg.K_s:
-        player_pos.y += 300 * dt
+        player_pos.y += 500 * dt
     if lastKeyPressed == pg.K_a:
-        player_pos.x -= 300 * dt
+        player_pos.x -= 500 * dt
     if lastKeyPressed == pg.K_d:
-        player_pos.x += 300 * dt
+        player_pos.x += 500 * dt
 
     screen.fill("black")  # Clear the screen with black
     pg.draw.rect(screen, (255, 0, 0), (player_pos.x, player_pos.y, 50, 50))
 
     pg.display.flip() 
+
+    if player_pos.x < 0 or player_pos.x > width or player_pos.y < 0 or player_pos.y > height:
+        screen.fill("black")
+        player_pos = pg.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+        pg.display.flip()
+        lastKeyPressed = None
+
 
     keys = pg.key.get_pressed()
     if keys[pg.K_w]:
