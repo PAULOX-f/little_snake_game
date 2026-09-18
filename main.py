9bbe91e5e31743cde3d709a5a756038c3fd0fddc
Paulo Alexandre <paulo.onf@gmail.com>
2026-09-18 00:39:55 -0300

    alteracao de movimento e sistema de grid e add contador de tempo para move

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-# pyright: reportMissingImports=false
 import pygame as pg
 
 pg.init()
 pg.display.set_caption("Snake")
 size = width, height = 1200, 800
+grid = 50
 screen = pg.display.set_mode(size)
 clock = pg.time.Clock()
 snake = [
-    pg.Vector2(600, 400),
-    pg.Vector2(550, 400),
-    pg.Vector2(500, 400)
-]
+    (12, 8),
+    (11, 8),
+    (10, 8)
+        ]
 dt = 0
 lastKeyPressed = None
 running = True
 
+move_timer = 0
+move_interval = 0.1
+
 while running:
-    ct = clock.tick(60)  # Limit to 60 FPS
+    ct = clock.tick(60)
+    snake_aux = snake.copy()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
@@ -31,25 +35,52 @@ while running:
             if event.key == pg.K_d:
                 lastKeyPressed = pg.K_d
 
+    move_timer += dt
+    print(move_timer)
     dt = ct / 1000.0  # Delta time in seconds
 
-    if lastKeyPressed == pg.K_w:
-        snake[0].y -= 300 * dt
-    if lastKeyPressed == pg.K_s:
-        snake[0].y += 300 * dt
-    if lastKeyPressed == pg.K_a:
-        snake[0].x -= 300 * dt
-    if lastKeyPressed == pg.K_d:
-        snake[0].x += 300 * dt
+    if move_timer >= move_interval:
+        move_timer = 0
+
+        if lastKeyPressed == pg.K_w:
+            x, y = snake[0]
+            snake[0] = (x, y - 1)
+
+            snake[1] = snake_aux[0]
+            snake[2] = snake_aux[1]
+
+        if lastKeyPressed == pg.K_s:
+            x, y = snake[0]
+            snake[0] = (x, y + 1)
+            snake[1] = snake_aux[0]
+            snake[2] = snake_aux[1]
+
+        if lastKeyPressed == pg.K_a:
+            x, y = snake[0]
+            snake[0] = (x - 1, y)
+            snake[1] = snake_aux[0]
+            snake[2] = snake_aux[1]
+            
+        if lastKeyPressed == pg.K_d:
+            x, y = snake[0]
+            snake[0] = (x + 1, y)
+
+            snake[1] = snake_aux[0]
+            snake[2] = snake_aux[1]
 
     screen.fill("black")  # Clear the screen with black
     for segment in snake:
-        pg.draw.rect(screen,(255, 0, 0),(segment.x, segment.y, 50, 50))
+        x, y = segment
+        pg.draw.rect(screen,(255, 0, 0),(x * grid, y * grid, grid, grid))
 
     pg.display.flip() 
 
-    if snake[0].x + 5 < 0 or snake[0].x + 45 > width or snake[0].y + 5 < 0 or snake[0].y + 45 > height:
-        snake[0] = pg.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    if snake[0][0] < 0 or snake[0][0] >= width // grid or snake[0][1] < 0 or snake[0][1] >= height // grid:
+        snake = [
+        (width // grid // 2, height // grid // 2),
+        (width // grid // 2 - 1, height // grid // 2),
+        (width // grid // 2 - 2, height // grid // 2)
+        ]
         lastKeyPressed = None
 
 
